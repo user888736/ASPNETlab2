2494e4068162d8dfe7145b602c51f54f02768629
Szymon Kwaśniewski <szymonkwasniewski@protonmail.com>
2021-11-16 20:16:58 +0100

    Adding new project

---
+++ WebMvc1/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc1.Models;
+
+namespace WebMvc1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Form1(FormModel model)
+        {
+            DateTime thisDay = DateTime.Today;
+
+            string userBirhday = model.Description;
+            DateTime Date = Convert.ToDateTime(userBirhday);
+
+            DateTime birthDays = Date;
+
+            if(Date.Date > thisDay.Date)
+            {
+                ViewBag.Result = "Błąd: ";
+                string numberOfDays = "Twoja data urodzin nie może być nowsza niż dzisiejsza data!";
+                ViewBag.DaysNumber = numberOfDays;
+            }
+            else
+            {
+                ViewBag.Result = "Od Twoich urodzin upłynęło: ";
+                int numberOfDays = (thisDay - birthDays).Days;
+                ViewBag.DaysNumber = numberOfDays;
+            }
+
+            
+
+
+            
+            return View(model);
+        }      
+
+    }
+}
---
+++ WebMvc1/Models/FormModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebMvc1.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Twoje imie: ")]
+        public string Name { get; set; }
+        [Display(Name = "Twoja data urodzenia: ")]
+        public string Description { get; set; }
+        
+    }
+}
